734449471c496e27ae277880c0e113f0cbd96267
PENG,CHAO-WEI <alanwei1202@gmail.com>
2019-02-22 14:36:43 +0800

    [add] add HandleUnknownAction

--- WebApplication1/Controllers/MVCController.cs
+++ WebApplication1/Controllers/MVCController.cs
@@ -8,6 +8,22 @@ namespace WebApplication1.Controllers
 {
     public class MVCController : Controller
     {
+        protected override void HandleUnknownAction(string actionName)
+        {
+            //Response.Redirect("/",true);
+            //base.HandleUnknownAction(actionName);
+            //try
+            //{
+            //    this.View(actionName).ExecuteResult(this.ControllerContext);
+            //}
+            //catch (InvalidOperationException ieox)
+            //{
+            //    ViewData["error"] = "Unknown Action: \"" +
+            //        Server.HtmlEncode(actionName) + "\"";
+            //    ViewData["exMessage"] = ieox.Message;
+            //    this.View("Error").ExecuteResult(this.ControllerContext);
+            //}
+        }
         // GET: MVC
         public ActionResult Index()
         {
